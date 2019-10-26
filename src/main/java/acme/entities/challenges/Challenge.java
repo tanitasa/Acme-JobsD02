9245cc60b3fe812e05ac6fdcd393bb6418ebeae5
damserfer <33392179+damserfer@users.noreply.github.com>
2019-10-26 15:40:59 +0200

    Atributo goals de Challenge correcto

--- src/main/java/acme/entities/challenges/Challenge.java
+++ src/main/java/acme/entities/challenges/Challenge.java
@@ -2,12 +2,17 @@
 package acme.entities.challenges;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.MapKeyColumn;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.Future;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 import acme.framework.entities.DomainEntity;
 import lombok.Getter;
@@ -35,8 +40,16 @@ public class Challenge extends DomainEntity {
 	private String				description;
 
 	//	@NotBlank
+	//	@ElementCollection(targetClass = String.class)
+	//	@MapKeyColumn(name = "goal")
 	//	@Pattern(regexp = "^GOLD|SILVER|BRONZE$")
 	//	private Map<String, Double>	goals;
+
+	@NotBlank
+	@Pattern(regexp = "^GOLD|SILVER|BRONZE$")
+	@ElementCollection(targetClass = String.class)
+	@MapKeyColumn(name = "goal")
+	private Map<String, Double>	goals				= new HashMap<String, Double>();
 
 	// Derived attributes -----------------------------------------------------
 
--- src/main/java/acme/features/authenticated/challenge/AuthenticatedChallengeShowService.java
+++ src/main/java/acme/features/authenticated/challenge/AuthenticatedChallengeShowService.java
@@ -34,7 +34,7 @@ public class AuthenticatedChallengeShowService implements AbstractShowService<Au
 		assert entity != null;
 		assert model != null;
 
-		request.unbind(entity, model, "title", "deadline", "description");
+		request.unbind(entity, model, "title", "deadline", "description", "goals");
 	}
 
 	@Override
