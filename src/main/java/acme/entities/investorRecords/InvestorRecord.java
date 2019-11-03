2a2de102d1e871f4650cfec553a12d0b409f9b6e
Victor Rosado <sinergiaftw@gmail.com>
2019-11-03 13:24:01 +0100

    last commit fix

---
+++ src/main/java/acme/entities/investorRecords/InvestorRecord.java
@@ -0,0 +1,38 @@
+
+package acme.entities.investorRecords;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import acme.framework.entities.DomainEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class InvestorRecord extends DomainEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	//Attributes ----------------------------------------------------------
+	@NotBlank
+	private String				name;
+
+	@NotBlank
+	private String				sector;
+
+	@NotBlank
+	private String				investingStatement;
+
+	@NotNull
+	@Min(0)
+	@Max(5)
+	private Integer				stars;
+
+}
